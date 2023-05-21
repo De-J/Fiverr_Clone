2ec4968e4f5d2ba57977b55b13a421fde7f4416f
Dev <devjtitan@gmail.com>
2023-05-21 10:50:24 +0530

    feat: add responsiveness to login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,52 +1,56 @@
 import { useState } from "react";
 import newRequest from "../../utils/newRequest.js";
 import { useNavigate } from "react-router-dom";
-import "./Login.scss";
+import "./Login.css";
 
 function Login() {
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
-    const navigate = useNavigate();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(null);
+	const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await newRequest.post("/auth/login", {
-                username,
-                password
-            });
-            localStorage.setItem("currentUser", JSON.stringify(res.data));
-            navigate("/");
-        }
-        catch(err) {
-            setError(err.response.data);
-        }
-    }
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await newRequest.post("/auth/login", {
+				username,
+				password
+			});
+			localStorage.setItem("currentUser", JSON.stringify(res.data));
+			navigate("/");
+		}
+		catch (err) {
+			setError(err.response.data);
+		}
+	}
 
-    return (
-        <div className="login">
-            <form onSubmit={handleSubmit}>
-                <h1>Sign in</h1>
-                
-                <label htmlFor="">Username</label>
-                <input 
-                    type="text" 
-                    placeholder="johndoe" 
-                    onChange={(e) => setUsername(e.target.value)}/>
-                
-                <label htmlFor="">Password</label>
-                <input
-                    type="password"
-                    placeholder="your password"
-                    onChange={(e) => setPassword(e.target.value)}/>
-                
-                <button type="submit">Login</button>
-                {error && error}
-            </form>
-        </div>
-    );
+	return (
+
+		<main className="login">
+			<h1>Sign in</h1>
+			<form onSubmit={handleSubmit}>
+				<label>
+					Username
+					<input
+						type="text"
+						placeholder="johndoe"
+						onChange={(e) => setUsername(e.target.value)} />
+				</label>
+
+				<label>
+					Password
+					<input
+						type="password"
+						placeholder="your password"
+						onChange={(e) => setPassword(e.target.value)} />
+				</label>
+
+				<button type="submit">Login</button>
+				{error && error}
+			</form>
+		</main>
+	);
 }
 
 export default Login;
