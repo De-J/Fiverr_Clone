7b81d5aabad5deed0b1300e7ac898d50c2c57b64
Dev <devjtitan@gmail.com>
2023-05-24 18:23:30 +0530

    fix: remove flexbox from css

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import newRequest from "../../utils/newRequest.js";
 import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import "./Login.scss";
 
 function Login() {
 
@@ -26,30 +26,29 @@ function Login() {
 	}
 
 	return (
-
-		<main className="login">
-			<h1>Sign in</h1>
+		<>
+		<main className="login">	
+			<div>
+				<h1>Sign in</h1>
+			</div>
 			<form onSubmit={handleSubmit}>
-				<label>
-					Username
-					<input
-						type="text"
-						placeholder="johndoe"
-						onChange={(e) => setUsername(e.target.value)} />
+				<label>Username
+				<input
+					type="text"
+					onChange={(e) => setUsername(e.target.value)} />
 				</label>
 
-				<label>
-					Password
-					<input
-						type="password"
-						placeholder="your password"
-						onChange={(e) => setPassword(e.target.value)} />
+				<label>Password
+				<input
+					type="password"
+					onChange={(e) => setPassword(e.target.value)} />
 				</label>
 
-				<button type="submit">Login</button>
+				<input type="submit" value="Sign in"/> 
 				{error && error}
 			</form>
 		</main>
+		</>
 	);
 }
 
