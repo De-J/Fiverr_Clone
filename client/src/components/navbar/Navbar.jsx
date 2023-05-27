12d809a1b4c3723c7c04a95cb9d21af162a0c0a3
Dev <devjtitan@gmail.com>
2023-05-27 16:03:51 +0530

    refactor: use map to remove repeated elements for category

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,135 +1,122 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import Logo from "../logo/Logo.jsx";
 import newRequest from "../../utils/newRequest.js";
 import "./Navbar.scss";
 
+const categories = [
+  "Graphics & Design",
+  "Video & Animation",
+  "Writing & Translation",
+  "AI Services",
+  "Digital Marketing",
+  "Music & Audio",
+  "Programming & Tech",
+  "Business",
+  "Lifestyle"
+]
+
+
 function Navbar() {
-    const [active, setActive] = useState(false);
-    const [open, setOpen] = useState(false);
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const { pathname } = useLocation();
-    const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
-    const isActive = () => {
-        window.scrollY > 0 ? setActive(true) : setActive(false);
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
     };
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener("scroll", isActive);
-        return () => {
-            window.removeEventListener("scroll", isActive);
-        };
-    }, []);
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-
-    const handleLogout = async () => {
-        try {
-            await newRequest.post("/auth/logout");
-            localStorage.setItem("currentUser", null);
-            navigate("/");
-        }
-        catch(err) {
-            console.log(err);
-        }
-
+  const handleLogout = async () => {
+    try {
+      await newRequest.post("/auth/logout");
+      localStorage.setItem("currentUser", null);
+      navigate("/");
     }
-    
-    return (
-        <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
-            <div className="container">
-                <div className="logo">
-                    <Link className="link" to="/">
-                        <span className="text">liverr</span>
-                    </Link>
-                    <span className="dot">.</span>
-                </div>
-            <div className="links">
-                <span>Liverr Business</span>
-                <span>Explore</span>
-                <span>English</span>
-                {!currentUser?.isSeller && <span>Become a Seller</span>}
-        
-        {currentUser ? (
-            <div 
-                className="user" 
-                onClick={()=>setOpen(!open)}>
-            
-                <img
-                    src={ currentUser.img || "/img/noavatar.jpg" }
-                    alt="noavatar"/>
+    catch (err) {
+      console.log(err);
+    }
 
-                <span>{currentUser?.username}</span>
-                {open && <div className="options">
-                
+  }
+
+  return (
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
+      <div className="container">
+        <Logo />
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+
+          {currentUser ? (
+            <div
+              className="user"
+              onClick={() => setOpen(!open)}>
+
+              <img
+                src={currentUser.img || "/img/noavatar.jpg"}
+                alt="noavatar" />
+
+              <span>{currentUser?.username}</span>
+              {open && <div className="options">
+
                 {currentUser.isSeller && (
-                    <>
+                  <>
                     <Link className="link" to="/mygigs">
-                    Gigs
+                      Gigs
                     </Link>
                     <Link className="link" to="/add">
-                    Add New Gig
+                      Add New Gig
                     </Link>
-                    </>
+                  </>
                 )}
                 <Link className="link" to="/orders">
-                Orders
+                  Orders
                 </Link>
                 <Link className="link" to="/messages">
-                Messages
+                  Messages
                 </Link>
                 <Link className="link" onClick={handleLogout}>
-                Logout
+                  Logout
                 </Link>
-                </div>}
+              </div>}
             </div>
-        ) : (
+          ) : (
             <>
-            <Link to="/login" className="link">Sign in</Link>
-            <Link className="link" to="/register">
-            <button>Join</button>
-            </Link>
+              <Link to="/login" className="link">Sign in</Link>
+              <Link className="link" to="/register">
+                <button>Join</button>
+              </Link>
             </>
-        )}
+          )}
         </div>
-        </div>
-        {(active || pathname !== "/") && (
-            <>
-            <hr />
-            <div className="menu">
-            <Link className="link menuLink" to="/">
-            Graphics & Design
-            </Link>
-            <Link className="link menuLink" to="/">
-            Video & Animation
-            </Link>
-            <Link className="link menuLink" to="/">
-            Writing & Translation
-            </Link>
-            <Link className="link menuLink" to="/">
-            AI Services
-            </Link>
-            <Link className="link menuLink" to="/">
-            Digital Marketing
-            </Link>
-            <Link className="link menuLink" to="/">
-            Music & Audio
-            </Link>
-            <Link className="link menuLink" to="/">
-            Programming & Tech
-            </Link>
-            <Link className="link menuLink" to="/">
-            Business
-            </Link>
-            <Link className="link menuLink" to="/">
-            Lifestyle
-            </Link>
-            </div>
-            <hr />
-            </>
-        )}
-        </div>
-    );
+      </div>
+      {(active || pathname !== "/") && (
+        <>
+          <hr />
+          <div className="menu">
+            {categories.map(category =>
+              <Link className="link menuLink" to="/">
+                {category}
+              </Link>
+            )}
+          </div>
+          <hr />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Navbar;
