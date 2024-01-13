8bead72411b6ac18c1121277d5b1235294108342
Dev <devjtitan@gmail.com>
2024-01-13 23:37:50 +0530

    add responsiveness to `<Featured/>`

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -3,28 +3,28 @@ import "./Featured.scss";
 function Featured() {
   return (
     <section className="featured">
-        <div className="content">
-          <h1>
-            Find the perfect <span>freelance</span> services for your business
-          </h1>
-          
-          <div className="search">
-            <input
-              style={{ width: "50%", borderRadius: "5px 0 0 5px" }}
-              type="text"
-              placeholder='Try "building mobile app"'
-            />
-            <button style={{ borderRadius: "0 5px 5px 0" }}>Search</button>
-          </div>
-          
-          <div className="popular">
-            <span>Popular:</span>
-            <button>Web Design</button>
-            <button>WordPress</button>
-            <button>Logo Design</button>
-            <button>AI Services</button>
-          </div>
+      <div className="content">
+        <h1>
+          Find the perfect <span>freelance</span> services for your business
+        </h1>
+
+        <div className="search">
+          <input
+            style={{ width: "50%", borderRadius: "5px 0 0 5px" }}
+            type="text"
+            placeholder='Try "building mobile app"'
+          />
+          <button style={{ borderRadius: "0 5px 5px 0" }}>Search</button>
         </div>
+
+        <div className="popular">
+          <span>Popular:</span>
+          <button>Web Design</button>
+          <button>WordPress</button>
+          <button>Logo Design</button>
+          <button>AI Services</button>
+        </div>
+      </div>
     </section>
   );
 }
