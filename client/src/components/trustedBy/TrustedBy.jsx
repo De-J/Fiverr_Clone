2207addff26d264a465ebba91020730a7a5c2328
Dev <devjtitan@gmail.com>
2024-01-17 23:30:09 +0530

    refactor `<TrustedBy/>`

--- client/src/components/trustedBy/TrustedBy.jsx
+++ client/src/components/trustedBy/TrustedBy.jsx
@@ -3,8 +3,7 @@ import "./TrustedBy.scss";
 
 const TrustedBy = () => {
   return (
-    <div className="trustedBy">
-      <div className="container">
+      <div className="trustedBy">
         <span>Trusted by:</span>
         <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook2x.188a797.png" alt="" />
         <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google2x.06d74c8.png" alt="" />
@@ -12,7 +11,6 @@ const TrustedBy = () => {
         <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg2x.6dc32e4.png" alt="" />
         <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal2x.22728be.png" alt="" />
       </div>
-    </div>
   );
 };
 
